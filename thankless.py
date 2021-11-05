e2f50d869514bc00ad1317bb86664d3d91fd66dd
Adam C Keith <adamchristopherkeith@gmail.com>
2021-11-04 20:19:18 -0400

    Add more state info and fix min_distance

--- thankless.py
+++ thankless.py
@@ -76,7 +76,7 @@ class SimpleState(GameState):
     # value of face up card, min card distance between players cards, and 
     # min card distance across all players
     
-    shape = (1,5)
+    shape = (1,6)
     
     def __init__(self):
         self.reset()
@@ -86,50 +86,75 @@ class SimpleState(GameState):
         self.shape = SimpleState.shape
         self.last_card = np.nan
         
+    @staticmethod
     def min_distance(player, card):
         """Calculate smallest difference between face up card and owned cards"""
         # the smallest absolute difference between face up card and cards 
-        # owned by this player. Even though having a lower neighboring card 
-        # is strictly better than a neighboring hire card, the difference is 1
-        # and not worth worrying about
+        # owned by this player, with the sign retained.
         # This is a way to measure if a card might be worth taking if it is
         # near other cards owned. 
         # if no cards, distance = np.nan.
         # special case, if card connects two other cards (in between) assign
         # distance of 0 because that is REALLY GOOD
         if (len(player.cards) > 0):
-            diffs = np.abs(card-player.cards)
-            distance = np.min(diffs)
-            if distance == 1:
-                # check if there are two distance == 1 and assign distance = 0
-                if np.count_nonzero(diffs == distance) == 2:
-                    distance = 0
-        else:
-            # distance = np.nan
-            # use a distance that gives ~10% chance of taking card so not to
-            # skew consensus pick early in the game
-            distance = 3
+            distance = card-player.cards
+            min_dist = min(distance, key=abs)
+            if -1*min_dist in distance:
+                # break ties negative, and check if this is the connection case
+                min_dist = abs(min_dist)
+                if min_dist == 1:
+                    min_dist = 0
+                else:
+                    min_dist *= -1
+
+            # print(player.position, player.cards, card, min_dist)
+            # abs_dist = np.abs(distance)
+            # mind_dist_ind = np.argmin(abs_dist)
             
-        return distance
+            # min_dist = distance[mind_dist_ind]
+            # min_abs_dist = abs_dist[mind_dist_ind]
+            
+            # # check if there are two distance == 1 and assign distance = 0
+            # if min_abs_dist == 1:
+            #     if np.count_nonzero(abs_dist == min_dist) == 2:
+            #         min_dist = 0
+        else:
+            min_dist = card
+        
+        return min_dist
         
     def update_state(self, player, game):
         if self.last_card == game.card:
             # only thing that could have changed is player.chips and game.chips
             self.state[0] -= 1 
             self.state[1] += game.number_of_players
+            self.state[2] += game.number_of_players
         else:
             # Only update distances if we haven't yet for this new card
             self.last_card = game.card
-        
+            # print("caller: ", player.position, self.last_card)
             # min_distance of this player
-            p_dist = SimpleState.min_distance(player, game.card)
+            p_min_dist = SimpleState.min_distance(player, game.card)
             
             # minimum min_distance of all other players        
-            o_dist = np.min([SimpleState.min_distance(p, game.card)
-                             for p in game.players if p is not player])
+            o_dists = [SimpleState.min_distance(p, game.card)
+                      for p in game.players if p is not player]
+            # this breaks ties negative
+            o_min_dist = min(o_dists, key=abs)
 
-            self.state=np.array([player.chips, game.chips, game.card, 
-                                 p_dist, o_dist])
+            card_reward = game.chips - game.card
+            if p_min_dist == 0:
+                # connecting runs, reward is reducing score by game.card+1 (+chips)
+                card_reward += 2*game.card+1 # add back game.card we subtracted
+            elif p_min_dist == -1:
+                # extra reward +1 because smaller end of a run
+                card_reward += game.card+1 
+            elif p_min_dist == 1:
+                # add back game.card we subtracted
+                card_reward += game.card 
+                
+            self.state=np.array([player.chips, game.chips, card_reward,
+                                 game.card, p_min_dist, o_min_dist])
         return self.state
         
 class FullState(GameState):
@@ -295,7 +320,7 @@ class Player(object):
                 self.reward = self.take_card(game)
             else:
                 self.reward = self.pass_turn()
-        self.reward += 1 # try making everything positive
+        # self.reward += 1 # try making everything positive
         return a
         
     def calculate_score(self):
@@ -317,11 +342,20 @@ class Human(Player):
         
         # wrap around all players back to ourself to print out their cards
         while game.next_player() is not self:
-            game.player.sort()
+            game.player.cards.sort()
             print("Player " + str(game.player.position) + "'s cards: " + str(game.player.cards))    
         
         return int(input("0 = Pass, 1 = Take Card: "))
+    
+    def game_over(self, game):
+        print("=========+++FINAL STATE=====================")
+        self.cards.sort()
+        print("My Cards: " + str(self.cards) + " My Chips: " + str(self.chips))
         
+        # wrap around all players back to ourself to print out their cards
+        while game.next_player() is not self:
+            game.player.cards.sort()
+            print("Player " + str(game.player.position) + "'s cards: " + str(game.player.cards))    
 
 class Computer(Player):
     """Generic automated player"""
@@ -342,7 +376,7 @@ class RandomAgent(Computer):
     def choose_action(self, game):
         return self.flip_coin(RandomAgent.p_take_card)
 
-class HeuristicAgent(Computer):
+class HeuristicAgent(RandomAgent):
     """Agent that uses some heuristics to make decisions"""
 
     def __init__(self, position):
@@ -352,10 +386,14 @@ class HeuristicAgent(Computer):
     def reset(self, position):
         super().reset(position)
         self.state.reset()
-    
+        
+    @staticmethod
     def prob_state(x, state_index):
         """Probability of taking the card based on the state variable"""
-        # [player.chips, game.chips, game.card, p_dist, o_dist])
+        # [player.chips, game.chips, game.card, take_reward, p_dist, o_dist])
+        
+        if np.isnan(x):
+            return x
         
         # player chips
         if state_index == 0:
@@ -363,13 +401,20 @@ class HeuristicAgent(Computer):
         # card chips
         elif state_index == 1:
             return sigmoid(x, 10, 0.6)
-        # card value
+        # reward for taking card
         elif state_index == 2:
+            # its ok to take small negative rewards?
+            return sigmoid(x, -3, 1)
+        # card value
+        elif state_index == 3:
             return 1-sigmoid(x, 18, 0.25)
         # player/opponent distance
-        elif state_index > 2:
-            return 1-sigmoid(x, 2, 2)
+        elif state_index > 3:
+            return 1-2*np.abs(sigmoid(x, 0, 0.5)-0.5)
+            # 1-sigmoid(np.abs(x), 2, 2)
         
+        
+    @staticmethod
     def heuristic_prob(state):
         """Return 0, p, or 1 based on consensus of coin flips for 
         various state variables"""
@@ -386,7 +431,7 @@ class HeuristicAgent(Computer):
         # no consensus err on the side of not taking the card by dropping the 
         # probability
         if np.isclose(consensus, 0.5):
-            consensus = RandomAgent.p_take_card
+            consensus = HeuristicAgent.p_take_card
         return consensus
         
     def choose_action(self, game):        
@@ -394,39 +439,69 @@ class HeuristicAgent(Computer):
         # with each coin being a function of the state variable
         self.state.update_state(self, game)
 
-        p = HeuristicAgent.heuristic_prob(self.state.state)
+        p = self.heuristic_prob(self.state.state)
 
         return self.flip_coin(p)
+
+class HeuristicAgent2(HeuristicAgent):
+    
+    @staticmethod
+    def heuristic_prob(state):
+        """Return 0, p, or 1 based on consensus of coin flips for 
+        various state variables"""
         
+        parray = np.array([HeuristicAgent.prob_state(value, i) 
+                           for i, value in enumerate(state)])
+
+        # split into chip state consensus and card state consensus
+        chip_ps = parray[0:3]
+        chip_flips = np.random.binomial(1, chip_ps)
+        chip_consensus = np.median(chip_flips)
+        
+        # this may contain nans
+        card_ps = parray[3:]
+        card_ps = card_ps[~np.isnan(card_ps)]
+        card_flips = np.random.binomial(1, card_ps)
+        card_consensus = np.median(card_flips)
+        
+        # This could be 0,0.25,0.5,0.75,1 which is ok?
+        consensus = np.median([chip_consensus, card_consensus])
+
+        return consensus
     
 class LearningAgent(Computer):
     
     gamma              = 0.95
     exploration_rate   = 1.0
     exploration_min    = 0.01
-    exploration_decay  = 0.995
+    exploration_decay  = 0.99
     
     def __init__(self, position):        
         self.state = SimpleState()
         self.memory = deque(maxlen=1000)
         self.set_eps(LearningAgent.exploration_rate)
+        self.set_is_training()
         
         super().__init__(position)
                 
         self.model = Sequential()
-        # self.model.add(Dense(16,activation='relu', input_dim=5))
-        self.model.add(Dense(20, activation='relu', input_dim=5))
-        self.model.add(Dense(20, activation='relu'))
+        self.model.add(Dense(24, activation='relu',
+                             input_dim=max(self.state.shape)))
+        self.model.add(Dense(24, activation='relu'))
         # self.model.add(Dense(2, activation='sigmoid'))
         self.model.add(Dense(2, activation='softmax'))
         self.model.compile(optimizer='adam', loss='mse')
         
     def reset(self, position):
         super().reset(position)
-        # Learn from the last game(s)
-        self.replay()
+        if self.is_training:
+            # Learn from the last game(s)
+            self.replay()
         self.last_action = None
         self.state.reset()
+    
+    def set_is_training(self, is_training = True):
+        self.is_training = is_training
         
     def set_eps(self, eps):
         if eps < LearningAgent.exploration_min:
@@ -435,23 +510,31 @@ class LearningAgent(Computer):
             self.eps = eps
             
     def game_over(self, game):
-        if self.last_action is not None:
+        if self.is_training and self.last_action is not None:
             self.remember(self.state.state, self.last_action, self.reward, None)
 
     def choose_action(self, game):
-        old_state = copy.deepcopy(self.state.state)
-        new_state = copy.deepcopy(self.state.update_state(self, game))
         
-        if self.last_action is not None:
-            self.remember(old_state, self.last_action, self.reward, new_state)
-
-        # Early in training, use heuristic agent
-        if np.random.random() < self.eps:
-            # p = HeuristicAgent.heuristic_prob(self.state.state)
-            # p_new = p
-            p_new = 0.8 # need to have a high chance of taking cards to get positive rewards sometimes?
+        if self.is_training:
+            old_state = copy.deepcopy(self.state.state)
+            new_state = copy.deepcopy(self.state.update_state(self, game))
+            
+            if self.last_action is not None:
+                self.remember(old_state, self.last_action, self.reward, new_state)
         else:
-            # p = self.model.predict(self.state.state.reshape(1,5))[0][1]
+            self.state.update_state(self, game)
+
+        # Early in training, use a quick heuristic agent
+        if self.is_training and np.random.random() < self.eps:
+            if self.state.state[2] > -2:
+                # positive rewards are very rare,
+                # and usually the best move is to pass which makes choosing
+                # positive rewards even harder, so help select them when learning
+                p_new = 0.5
+            else:
+                # usually a good move to pass
+                p_new = 0.1 
+        else:
             p = self.model.predict(self.state.state.reshape(self.state.shape))[0]
             p_new = np.argmax(p)
         # min_p = 1e-5
